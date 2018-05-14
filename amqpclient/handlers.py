1ff06b5c9ae4f903601a2116a38b5cefe3d7d501
David Marteau <david@innophi.com>
2018-05-14 21:41:15 +0200

    Use MPL licence

--- amqpclient/handlers.py
+++ amqpclient/handlers.py
@@ -1,7 +1,11 @@
 #
-# Copyrights 2017 3Liz
-# Author: David Marteau (dmarteau@3liz.com)
+# Copyright 2017 3liz
+# Author David Marteau
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """
 AMQP RPC handler dispatcher
 
--- amqpclient/logger.py
+++ amqpclient/logger.py
@@ -1,8 +1,11 @@
-# -*- encoding=utf-8 -*-
 #
-# Copyrights 2016-2017 3Liz
-# Author: David Marteau (dmarteau@3liz.com)
+# Copyright 2017 3liz
+# Author David Marteau
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """
 Define a logger based on amqp
 """
